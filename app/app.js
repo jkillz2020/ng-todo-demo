2f3ddeffb9386c4fa4cd03536f77c65c35cb0b18
jkillz2020 <kilgore.josh@gmail.com>
2016-05-21 09:26:00 -0500

    sass compiling css and gulp working

--- app/app.js
+++ app/app.js
@@ -1,5 +1,10 @@
+"use strict";
 var app = angular.module("TodoApp", []);
 
+app.controller("NavCtrl", function($scope){
+  $scope.navItems = [{name: "Logout"}, {name: "All Items"}, {name: "New Item"}];
+});
+
 app.controller("TodoCtrl", function($scope){
-  $scope.welcome = "hello"
-})
+  $scope.welcome = "hello";
+});
