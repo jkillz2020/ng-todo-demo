567d4be81b3ef8a651622857613f4a628c77f951
jkillz2020 <kilgore.josh@gmail.com>
2016-05-21 10:54:10 -0500

    to do form added

--- app/app.js
+++ app/app.js
@@ -7,4 +7,39 @@ app.controller("NavCtrl", function($scope){
 
 app.controller("TodoCtrl", function($scope){
   $scope.welcome = "hello";
+  $scope.showListView = true;
+  $scope.newTask = []
+
+  $scope.items = [
+  {
+    id: 0,
+    task: 'mow the lawn',
+    isCompleted: true,
+    dueDate: '12/5/17',
+    assignedTo: 'greg',
+    location: "Zoe's house",
+    urgency: "low",
+    dependencies: ["sunshine", "clippers", "hat", "water", "headphones"]
+  },
+  {
+    id: 1,
+    task: 'grade quizzes',
+    isCompleted: false,
+    dueDate: '12/5/16',
+    assignedTo: 'joe',
+    location: "NSS",
+    urgency: "low",
+    dependencies: ["wifi", "vodka", "tissues", "nap", "headphones"]
+  }
+]
+
+   $scope.newItem = function(){
+        console.log("you hit new item");
+        $scope.showListView = false;
+    };
+    $scope.allItem = function(){
+        console.log("you hit all items");
+        $scope.showListView = true;
+
+    };
 });
