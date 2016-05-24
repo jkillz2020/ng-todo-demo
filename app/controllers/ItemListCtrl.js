61342b12076e614ddad5f4be82239460b119a678
jkillz2020 <kilgore.josh@gmail.com>
2016-05-23 19:36:25 -0500

    add delete button functionality

--- app/controllers/ItemListCtrl.js
+++ app/controllers/ItemListCtrl.js
@@ -1,5 +1,7 @@
-app.controller("ItemListCtrl", function($scope, $http){
+app.controller("ItemListCtrl", function($scope, $http, $location){
   $scope.items = [];
+  var getItems = function(){
+
 
   $http.get("https://todo-angular-js.firebaseio.com/items.json")
   .success(function(itemObject){
@@ -9,6 +11,18 @@ app.controller("ItemListCtrl", function($scope, $http){
       itemCollection[key].id=key;
       $scope.items.push(itemCollection[key]);
     })
-  })
+  });
+}
+getItems();
+  $scope.itemDelete = function(itemId){
+    console.log("itemId", itemId);
+    $http
+      .delete(`https://todo-angular-js.firebaseio.com/items/${itemId}.json`)
+      .success(function(response){
+        console.log(response);
+        $scope.items = [];
+        getItems();
+      })
+  }
     
 });
